635f8112f72b24735ff2feb0f94a5ebd0abe95cb
deveshwadibhasme <146552994+deveshwadibhasme@users.noreply.github.com>
2024-11-09 14:03:02 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function scrolling() {
     document.body.style.overflow = 'visible'
 }
 window.addEventListener('load', () => {
-    setTimeout(loader, 2000)
-    setTimeout(scrolling,2500)
+    loader();
+    setTimeout(scrolling,1000)
 })
 
 const hoverDiv = document.querySelectorAll('.custom');
@@ -76,73 +76,6 @@ const applicantEducation = document.querySelector('#education')
 const applicantNumber = document.querySelector('#number')
 const applicantAddress = document.querySelector('#address')
 
-
-// let keyStoreName = []
-// applicantName.addEventListener('change', (key) => {
-//     const uniqueKey = `${`Name`}${new Date().getTime()}`
-//     localStorage.setItem(uniqueKey,
-//         key.target.value
-//     )
-//     keyStoreName.push(uniqueKey)
-// })
-// let keyStoreEducation = []
-// applicantEducation.addEventListener('change', (key) => {
-//     const uniqueKey = `${`Education`}${new Date().getTime()}`
-//     localStorage.setItem(uniqueKey,
-//         key.target.value
-//     )
-//     keyStoreEducation.push(uniqueKey)
-// })
-// let keyStoreNumber = []
-// applicantNumber .addEventListener('change', (key) => {
-//     const uniqueKey = `${`Number`}${new Date().getTime()}`
-//     localStorage.setItem(uniqueKey,
-//         key.target.value
-//     )
-//     keyStoreNumber.push(uniqueKey)
-// })
-
-
-// // applicantEducation.addEventListener('input', (key) => {
-// //     localStorage.setItem('education', key.target.value)
-// // })
-// // applicantNumber.addEventListener('input', (key) => {
-// //     localStorage.setItem('number', key.target.value)
-// // })
-// applicantAddress.addEventListener('input', (key) => {
-//     localStorage.setItem('address', key.target.value)
-// })
-
-
-// let tableData = document.querySelector('#table-data')
-// let localName = ''
-// function showValue() {
-//     const div = document.createElement('div')
-//     div.classList.add('grid')
-//     div.classList.add('grid-cols-4')
-//     for (let i = 0; i < keyStoreName.length; i++) {
-//         const keyHold = keyStoreName[i]
-//         localName = localStorage.getItem(`${keyHold}`)
-//     }
-//     let localEducation
-//     for (let i = 0; i < keyStoreEducation.length; i++) {
-//         const keyHold = keyStoreEducation[i]
-//         localEducation = localStorage.getItem(`${keyHold}`)
-//     }
-//     let localNumber
-//     for (let i = 0; i < keyStoreNumber.length; i++) {
-//         const keyHold = keyStoreNumber[i]
-//         localNumber = localStorage.getItem(`${keyHold}`)
-//     }
-
-//     div.innerHTML = `<div>${localName}</div>
-//                     <div>${localEducation}</div>
-//                     <div>${localNumber}</div>
-//                     <div>${localStorage.getItem('address')}</div>`
-//     entryList.appendChild(div)
-// }
-
-
 function showValue(){
    const form =  document.forms['submit-to-google-sheet']
    setTimeout(()=>{
@@ -163,3 +96,4 @@ fetch('data.json').then((res) => res.json()).then((data) => {
     });
 }
 )
+
